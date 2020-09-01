6f571575143dd70513492a2dd1baa523129993b1
unknown <rushi.khot05@gmail.com>
2020-09-02 02:49:22 +0530

    second commit

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import {
+    GET_ERRORS,
+    SET_CURRENT_USER,
+    USER_LOADING
+} from "./types"
+
+//Register the user
+export const registerUser = (userData, history) => dispatch => {
+    axios
+        .post("./api/users/register", userData)
+        .then(res => history.push("/login")) //redirect to login on successful register
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        ); 
+};
+
+//Login - Get user token
+export const loginUser = userData => dispatch => {
+    axios
+        .post("./api/users/login", userData)
+        .then(res => {
+            //Save to local storage
+            //Set token to local storage
+            const { token } = res.data;
+            localStorage.setItem("jwtToken", token);
+            //Set token to auth header
+            setAuthToken(token);
+            //Decode token to get user data
+            const decoded = jwt_decode(token);
+            //Set current user
+            dispatch(setCurrentUser(decoded));
+        })
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
+
+//Set logged in user
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    };
+};
+
+//User Loading
+export const setUserLoading = () => {
+    return {
+        type: USER_LOADING
+    };
+};
+
+//Log user out
+export const logoutUser = () => dispatch => {
+    //Remove token from local storage
+    localStorage.removeItem("jwtToken");
+    //Remove auth header for future requests
+    setAuthToken(false);
+    //Set current user to empty object {} which will set isAuthenticated to false
+    dispatch(setCurrentUser({}));
+};
